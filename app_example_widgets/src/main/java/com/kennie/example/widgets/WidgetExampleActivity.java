36e580badd0f18ac9bceed80ff40492f4c88d141
kennie <245794335@qq.com>
2022-01-19 12:01:14 +0800

    【Init】 初始化Widgets库及演示项目

---
+++ app_example_widgets/src/main/java/com/kennie/example/widgets/WidgetExampleActivity.java
@@ -0,0 +1,19 @@
+package com.kennie.example.widgets;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+/**
+ * 演示Widgets
+ */
+public class WidgetExampleActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_widget_example);
+    }
+}
