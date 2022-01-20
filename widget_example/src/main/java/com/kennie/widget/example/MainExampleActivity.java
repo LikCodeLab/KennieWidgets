d66bb602519e41b8afe2b770ae2c7356b2eec47b
kennie <245794335@qq.com>
2022-01-20 15:21:37 +0800

    update develop-base

---
+++ widget_example/src/main/java/com/kennie/widget/example/MainExampleActivity.java
@@ -0,0 +1,15 @@
+package com.kennie.widget.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_example);
+    }
+}
